7f2018c078bbd2b8859554462e1e9ba288f04e23
Lory <lory@voodoo.io>
2019-01-21 14:10:04 +0100

    better handling of pull to dismiss transition

--- CleanyModal/Classes/CleanyModalViewController.swift
+++ CleanyModal/Classes/CleanyModalViewController.swift
@@ -40,38 +40,25 @@ open class CleanyModalViewController: UIViewController {
     @IBAction open func handleGesture(_ sender: UIPanGestureRecognizer) {
 
         let percentThreshold: CGFloat = 0.15
-
-        // convert y-position to downward pull progress (percentage)
-        let translation = sender.translation(in: self.view)
+      
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
         let verticalMovement = translation.y / self.view.bounds.height
-        let downwardMovement = fmaxf(Float(verticalMovement), 0.0)
-        let downwardMovementPercent = fminf(downwardMovement, 1.0)
-        let progress = CGFloat(downwardMovementPercent)
-        
+        let progress = min(max(verticalMovement, 0.0), 1.0)
+
         guard let interactor = transitionInteractor else { return }
-        
         switch sender.state {
-        case .began:
-            interactor.hasStarted = true
-            onDismissCallback?(self)
-            dismiss(animated: true, completion: nil)
-            // glitch fix https://stackoverflow.com/questions/48666523/glitch-when-interactively-dismiss-a-modal-view-controller/50238562#50238562
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.001) {
-                interactor.update(progress)
+        case .began, .changed:
+            if velocity.y > 0 && interactor.hasStarted == false {
+                interactor.hasStarted = true
+                dismiss(animated: true, completion: nil)
             }
-        case .changed:
-            interactor.shouldFinish = progress > percentThreshold
+            interactor.shouldFinish = (progress > percentThreshold) && velocity.y > 0
             interactor.update(progress)
-        case .cancelled:
-            interactor.hasStarted = false
-            interactor.cancel()
-        case .ended:
-            interactor.hasStarted = false
-            interactor.shouldFinish
-                ? interactor.finish()
-                : interactor.cancel()
+            interactor.completionSpeed = 1.0 - interactor.percentComplete
         default:
-            break
+            interactor.hasStarted = false
+            interactor.shouldFinish ? interactor.finish() : interactor.cancel()
         }
     }
     
