514ca566339377bafb19bc9a64c466cf8a35a4a0
Gautier Gédoux <gautier.gedoux@isep.fr>
2020-04-02 14:09:44 +0200

    Update CleanyModalViewController.swift

--- CleanyModal/Classes/CleanyModalViewController.swift
+++ CleanyModal/Classes/CleanyModalViewController.swift
@@ -22,7 +22,7 @@ open class CleanyModalViewController: UIViewController {
     @IBOutlet public var alertViewCenterY: NSLayoutConstraint!
     @IBOutlet public var alertWidthConstraint: NSLayoutConstraint!
     @IBOutlet public var alertViewBottom: NSLayoutConstraint?
-        
+    
     public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
